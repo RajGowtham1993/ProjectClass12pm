b0701570e38d23aa1a258bf055b6f5d71d14762f
RajGowtham1993 <RajGowtham1993@gmail.com>
2023-04-16 19:08:29 +0530

    3 methods

---
+++ src/test/java/com/hcl/Methods.java
@@ -0,0 +1,17 @@
+package com.hcl;
+
+public class Methods {
+	private void test1() {
+		System.out.println("test1");
+
+	}
+	private void test2() {
+		System.out.println("test2");
+
+	}
+	private void test3() {
+		System.out.println("test3");
+
+	}
+
+}
